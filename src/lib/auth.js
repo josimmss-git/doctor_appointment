52b457d6e95fc4dcf5b7073da8a9ded9581bb4e8
josimmss-git <josimmss2021@gmail.com>
2026-06-16 12:39:31 +0600

    added google auth

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,14 +3,24 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI);
-const db = client.db('doctor_appointment');
+
+await client.connect();
+
+const db = client.db("doctor_ppointment");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
-    client
+    client,
   }),
+
   emailAndPassword: {
-  enabled:true,
-}
-});
+    enabled: true,
+  },
+
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    },
+  },}
+);
